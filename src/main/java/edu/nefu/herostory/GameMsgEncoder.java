8b59349bea348aa75c7ef4963bf381fafc6b4850
wenbinai <208176146@qq.com>
2021-04-28 10:45:05 +0800

    修改了用户下线出现重复用户的bug

--- src/main/java/edu/nefu/herostory/GameMsgEncoder.java
+++ src/main/java/edu/nefu/herostory/GameMsgEncoder.java
@@ -29,6 +29,8 @@ public class GameMsgEncoder extends ChannelOutboundHandlerAdapter {
             msgCode = GameMsgProtocol.MsgCode.WHO_ELSE_IS_HERE_RESULT_VALUE;
         } else if (msg instanceof GameMsgProtocol.UserMoveToResult) {
             msgCode = GameMsgProtocol.MsgCode.USER_MOVE_TO_RESULT_VALUE;
+        } else if (msg instanceof GameMsgProtocol.UserQuitResult) {
+            msgCode = GameMsgProtocol.MsgCode.USER_QUIT_RESULT_VALUE;
         } else {
             LOGGER.error("无法识别的消息类型, msgClazz = " + msg.getClass().getName());
             return;
